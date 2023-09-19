9372b0c79bcd575c3ccaf6c87a842b0b85f0d506
Lê Tấn Khôi <43715386+HelpMe-Pls@users.noreply.github.com>
2023-09-19 15:19:02 +0700

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,13 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
+
+import react from '@astrojs/react'
+import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	/* Dev mode */
+	server: {
+		port: 6996,
+	},
+	integrations: [react(), tailwind()],
+})
